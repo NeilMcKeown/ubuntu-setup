3680dfa50d17871d9bde28f88706fff848a4c351
Neil McKeown <neilsimonmckeown@gmail.com>
2017-02-26 22:11:30 +0000

    Remove Intellij, in favour of IDEA Toolbox later. Add Yarn env value.

--- setup.sh
+++ setup.sh
@@ -43,6 +43,7 @@ sudo apt-get install -y atom
 echo "###################################################################################################"
 echo "# Installing Yarn..."
 echo "###################################################################################################"
+cd ~
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
@@ -106,61 +107,5 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -f install
 
-echo "###################################################################################################"
-echo "Installing Intellij..."
-echo "###################################################################################################"
-
-# We need root to install
-#[ $(id -u) != "0" ] && exec sudo "$0" "$@"
-
-# Prompt for edition
-while true; do
-    read -p "Enter 'U' for Ultimate or 'C' for Community: " ed
-    case $ed in
-        [Uu]* ) ed=U; break;;
-        [Cc]* ) ed=C; break;;
-    esac
-done
-
-# Fetch the most recent version
-VERSION=$(wget "https://www.jetbrains.com/intellij-repository/releases" -qO- | grep -P -o -m 1 "(?<=https://www.jetbrains.com/intellij-repository/releases/com/jetbrains/intellij/idea/BUILD/)[^/]+(?=/)")
-
-# Prepend base URL for download
-URL="https://download.jetbrains.com/idea/ideaI$ed-$VERSION.tar.gz"
-
-echo $URL
-
-# Truncate filename
-FILE=$(basename ${URL})
-
-# Set download directory
-DEST=~/Downloads/$FILE
-
-echo "Downloading idea-I$ed-$VERSION to $DEST..."
-
-# Download binary
-wget -cO ${DEST} ${URL} --read-timeout=5 --tries=0
-
-echo "Download complete!"
-
-# Set directory name
-DIR="/opt/idea-I$ed-$VERSION"
-
-echo "Installing to $DIR"
-
-# Untar file
-if sudo mkdir ${DIR}; then
-    sudo tar -xzf ${DEST} -C ${DIR} --strip-components=1
-fi
-
-# Grab executable folder
-BIN="$DIR/bin"
-
-# Add permissions to install directory
-sudo chmod -R +rwx ${DIR}
-
-# Create symlink entry
-sudo ln -sf ${BIN}/idea.sh /usr/local/bin/idea
-
-echo "Done."
-whoami
+echo "Add the following line to your .zshrc / .bashrc etc."
+echo "export PATH="$PATH:yarn global bin:$HOME/.config/yarn/global/node_modules/.bin"
