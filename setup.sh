4b25fcf5f017d9e2e89e035cb1b241b2fd642540
Neil McKeown <neilsimonmckeown@gmail.com>
2017-03-07 22:33:09 +0000

    Cloud Foundry

--- setup.sh
+++ setup.sh
@@ -54,7 +54,20 @@ echo "# Installing Yeoman..."
 echo "###################################################################################################"
 yarn global add yo
 
+echo "###################################################################################################"
+echo "# Installing JHipster..."
+echo "###################################################################################################"
 yarn global add generator-jhipster
+
+echo "###################################################################################################"
+echo "# Installing Cloud Foundry CLI..."
+echo "###################################################################################################"
+# ...first add the Cloud Foundry Foundation public key and package repository to your system
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+# ...then, update your local package index, then finally install the cf CLI
+sudo apt-get update
+sudo apt-get install cf-cli
 
 echo "###################################################################################################"
 echo "# Installing Docker..."
